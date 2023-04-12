4bdbf6dfc45c0535d9a432a44962016d88a45703
sundelbokong <101465523+sundelbokong@users.noreply.github.com>
2023-04-13 06:23:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -154,9 +154,9 @@ app.get('/contact', async (req, res) => {
 
 app.get('/video', async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  // res.setHeader('Access-Control-Allow-Origin', '*')
   // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
   res.setHeader('Access-Control-Allow-Methods', 'GET')
   res.setHeader(
     'Access-Control-Allow-Headers',
