027aedeaf701ead58e66b73dac4ae8def6ef8fa7
sundelbokong <101465523+sundelbokong@users.noreply.github.com>
2023-04-13 06:21:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -153,6 +153,15 @@ app.get('/contact', async (req, res) => {
 })
 
 app.get('/video', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   // Ensure there is a range given for the video
   const range = req.headers.range
   if (!range) {
