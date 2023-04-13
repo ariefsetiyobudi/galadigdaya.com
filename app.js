219dac11ff22a86917b1a48d47c2b09e928de748
sundelbokong <101465523+sundelbokong@users.noreply.github.com>
2023-04-13 07:31:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -153,9 +153,6 @@ app.get('/contact', async (req, res) => {
 })
 
 app.get('/video', async (req, res) => {
-  const ua = UAParser(req.headers['user-agent'])
-  const browser = ua.browser.name
-  const model = ua.browser.name
   // Ensure there is a range given for the video
   const range = req.headers.range
   if (!range) {
@@ -166,55 +163,29 @@ app.get('/video', async (req, res) => {
   const videoPath = 'public/pexels-kelly-lacy-6595364.mp4'
   const videoSize = fs.statSync(videoPath).size
 
-  if (browser === 'Safari' || browser === 'Mobile Safari' || model === 'iPhone') {
-    // Parse Range
-    // Example: "bytes=32324-"
-    const CHUNK_SIZE = range.replace(/bytes=/, '').split('-')
-    const start = parseInt(CHUNK_SIZE[0], 10)
-    const end = CHUNK_SIZE[1] ? parseInt(CHUNK_SIZE[1], 10) : videoSize - 1
+  // Parse Range
+  // Example: "bytes=32324-"
+  const CHUNK_SIZE = 10 ** 6 // 1MB
+  const start = Number(range.replace(/\D/g, ''))
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
 
-    // Create headers
-    const contentLength = end - start + 1
-    const headers = {
-      'Content-Range': `bytes ${start}-${end}/${videoSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': contentLength,
-      'Content-Type': 'video/mp4'
-    }
-
-    // HTTP Status 206 for Partial Content
-    res.writeHead(206, headers)
-
-    // create video read stream for this particular chunk
-    const videoStream = fs.createReadStream(videoPath, { start, end })
-
-    // Stream the video chunk to the client
-    videoStream.pipe(res)
-  } else {
-    // Parse Range
-    // Example: "bytes=32324-"
-    const CHUNK_SIZE = 10 ** 6 // 1MB
-    const start = Number(range.replace(/\D/g, ''))
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
-
-    // Create headers
-    const contentLength = end - start + 1
-    const headers = {
-      'Content-Range': `bytes ${start}-${end}/${videoSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': contentLength,
-      'Content-Type': 'video/mp4'
-    }
-
-    // HTTP Status 206 for Partial Content
-    res.writeHead(206, headers)
-
-    // create video read stream for this particular chunk
-    const videoStream = fs.createReadStream(videoPath, { start, end })
-
-    // Stream the video chunk to the client
-    videoStream.pipe(res)
+  // Create headers
+  const contentLength = end - start + 1
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': contentLength,
+    'Content-Type': 'video/mp4'
   }
+
+  // HTTP Status 206 for Partial Content
+  res.writeHead(206, headers)
+
+  // create video read stream for this particular chunk
+  const videoStream = fs.createReadStream(videoPath, { start, end })
+
+  // Stream the video chunk to the client
+  videoStream.pipe(res)
 })
 
 app.listen(port, () => {
