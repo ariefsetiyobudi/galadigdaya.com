da5efe0b6fd46c7b18a4af9f0108b72245706120
ariefsetiyobudi <ariefsetiyobudi@me.com>
2023-10-12 07:46:48 +0700

    Update Dependencies

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 require('dotenv').config()
 
-const fetch = require('node-fetch')
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 const logger = require('morgan')
 const errorHandler = require('errorhandler')
 const bodyParser = require('body-parser')
